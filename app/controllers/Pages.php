56d8551bf3279d4e5e16860f51f5c49f2f487926
yungfarz <ajanifarouq@gmail.com>
2022-04-05 11:12:47 +0100

    first commit

---
+++ app/controllers/Pages.php
@@ -0,0 +1,15 @@
+<?php
+
+class Pages {
+    public function __construct(){
+       // echo "Loaded";
+    }
+
+    public function index(){
+        echo "Home Page";
+    }
+
+    public function about(){
+        echo "about";
+    }
+}
---
+++ app/require.php
@@ -0,0 +1,11 @@
+<?php
+
+     //Require libraries from folder Libraries
+     require_once 'libraries/Core.php';
+     require_once 'libraries/Controller.php';
+     require_once 'libraries/Database.php';
+
+     
+     //Instantiate Core Class
+     $init = new Core();
+     
